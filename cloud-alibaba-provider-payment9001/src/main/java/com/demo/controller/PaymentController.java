9f55bb97a58aa096cab630aeac782c70eb2032d5
chenhao <chenhao_1919@163.com>
2020-05-07 21:25:38 +0800

    nacos config

--- cloud-alibaba-provider-payment9001/src/main/java/com/demo/controller/PaymentController.java
+++ cloud-alibaba-provider-payment9001/src/main/java/com/demo/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2020/5/6 22:20
  */
 @RestController
+@RefreshScope
 public class PaymentController {
     @Value("${server.port}")
     private Integer serverPort;
 
+    @Value("${config.info}")
+    private String configInfo;
+
     @GetMapping("/echo/{name}")
     public String echo(@PathVariable String name){
         return "["+ serverPort +"] Hello, " + name;
+    }
+
+    @GetMapping("/config-info")
+    public String getConfigInfo(){
+        return this.configInfo;
     }
 }
